7718a91962954da6b71f20c4176e4c0e18f615be
iiom3 <115708513+iiom3@users.noreply.github.com>
2022-11-02 10:40:54 +0200

    created a counter

---
+++ main.js
@@ -0,0 +1,51 @@
+// console.log("hello there how is it going")
+// console.error("Error Here!");
+// console.log("He is %cReally %c Smart", "color: red", "color: green");
+// console.table(["one", "two", "three"])
+
+// document.write("<h1>elzero</h1>");
+// document.querySelector("h1").style.color = "blue";
+// document.querySelector("h1").style.fontSize = "80px";
+// document.querySelector("h1").style.fontWeight = "bold";
+// document.querySelector("h1").style.textAlign = "center";
+// document.querySelector("h1").style.fontFamily = "Arial";
+// document.querySelector("h1").style.textTransform = "capitalize";
+
+
+// console.log("%cElzero %cWeb %cSchool", "font-size: 40px; color: red", "font-size: 40px; color: green; font-weight: bold", "font-size: 40px; color: #fff; background-color: blue;");
+// console.groupCollapsed("Group 1");
+//   console.log("Message One");
+//   console.log("Message Two");
+//     console.groupCollapsed("Child Group");
+//       console.log("Message One");
+//       console.log("Message Two");
+//         console.groupCollapsed("Grand Child Group");
+//           console.log("Message One");
+//           console.log("Message Two");
+//         console.groupEnd();
+//     console.groupEnd();
+// console.groupEnd();
+// console.groupCollapsed("Group 2");
+//   console.log("Message One");
+//   console.log("Message Two");
+// console.groupEnd();
+
+// console.table(["Omar", "Elzero", "Ali", "Sami", "Ahmad"])
+
+// let title = "Elzero";
+// let paragraph = "Elzero Web School";
+
+// let markup = `
+//   <div class="card">
+//     <div class="child">
+//       <h2>${title}</h2>
+//       <p>${paragraph}</p>
+//     </div>
+//   </div>
+// `;
+
+// document.write(markup);
+
+// console.log("40" - 10);
+
+console.log(Math.trunc((Math.random()*101) + 20));
